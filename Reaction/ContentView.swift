cc070002e9c2866df8281a1d8c17ed59f3d2323e
Ping-Sung <76198720+Ping-Sung@users.noreply.github.com>
2025-11-03 17:05:27 -0500

    Change color from red and green

--- Reaction/ContentView.swift
+++ Reaction/ContentView.swift
@@ -9,14 +9,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var color: Color = .red
+    @State private var timer: Timer?
+    @State private var nextChange: Double = 0.0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            color
+                .ignoresSafeArea()
+                .onAppear(perform: startColorCycle)
+
+            VStack {
+                Spacer()
+                Button("Restart") {
+                    restartCycle()
+                }
+                .padding()
+                .background(Color.white)
+                .foregroundColor(.black)
+                .cornerRadius(12)
+                .padding(.bottom, 40)
+            }
         }
-        .padding()
+    }
+
+    // 每 2～5 秒隨機變色
+    func startColorCycle() {
+        scheduleNextChange()
+    }
+
+    func scheduleNextChange() {
+        nextChange = Double.random(in: 2...5)
+        
+        timer = Timer.scheduledTimer(withTimeInterval: nextChange, repeats: false) { _ in
+            toggleColor()
+            scheduleNextChange()
+        }
+    }
+
+    func toggleColor() {
+        color = (color == .red) ? .green : .red
+    }
+
+    func restartCycle() {
+        timer?.invalidate()
+        color = .red
+        startColorCycle()
     }
 }
 
